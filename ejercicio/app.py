c2f80c8b40a8db107bd2342d92b521c776450a57
S11pedroza <spedroza@utpl.edu.ec>
2024-04-28 23:18:41 -0500

    cambios

--- ejercicio/app.py
+++ ejercicio/app.py
@@ -1,3 +1,13 @@
+import os
+
+
+# Definir el nombre del directorio donde se guardarán los archivos HTML
+directorio_salida = "ejercicio/data/reportes"
+
+# Crear el directorio si no existe
+if not os.path.exists(directorio_salida):
+    os.makedirs(directorio_salida)
+
 # proceso 
 #
 # acceder al archivo
@@ -8,9 +18,6 @@ lineas = archivo.readlines()
 
 # lineas es ina lista de cadenas
 # se imprime las siguientes posiciones
-print(lineas[0])
-print(lineas[1])
-
 encabezados = lineas[0]
 encabezados = encabezados.split("|")
 # en línea tomo el valor de lineas[1]
@@ -20,31 +27,39 @@ linea = lineas[1]
 # Recuerdo que el separador de la cadena es un "|"
 
 linea = linea.split("|")
-
 # imprimo la nueva línea; que ahora es una lista
-print(linea)
 
 archivo.close()
 
-for Linea in lineas:
-  print(Linea)
-  pagina = """
-  <!DOCTYPE html>
-  <html lang="en" dir="ltr">
-    <head>
-      <meta charset="utf-8">
-      <title></title>
-    </head>
-    <body>
-    <b>%s:</b> %s  
-    </body>
-    </html>
-  """ % (encabezados[1], linea[1])
 
-  archivo_generado = open("ejercicio/data/%s.html" % linea[0], "w")
-  
-  archivo_generado.close()
+# Recorrer todas las líneas del archivo excepto la primera (que son los encabezados)
+for linea in lineas[1:]:
+    # Separar los valores de la línea
+    valores = linea.split("|")
+    # Inicializar la variable para el HTML de esta página
+    pagina = """
+        <!DOCTYPE html>
+        <html lang="en" dir="ltr">
+        <head>
+            <meta charset="utf-8">
+            <title>Trabajo Grupal</title>
+        </head>
+        <body> 
+        
+        """
 
-  print(pagina)
+    # Iterar sobre los encabezados y los valores
+    for encabezado, valor in zip(encabezados, valores):
+        # Agregar cada par encabezado-valor al HTML
+         pagina += "<b>{}</b>: {}<br>".format(encabezado, valor)
 
+        # Cerrar la etiqueta body y html
+    pagina += """
+        </body>
+        </html>
+        """
+
+        # Escribir el HTML en un archivo correspondiente al código AMIE de la institución
+    with open(directorio_salida+"/{}.html".format(valores[0]), "w") as archivo_generado:
+            archivo_generado.write(pagina)
 
